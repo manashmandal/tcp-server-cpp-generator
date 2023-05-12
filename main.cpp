aa779d4396e0680ff507c09d8a1e4ae85013329b
manashmandal <manashmndl@gmail.com>
2023-05-12 02:02:26 +0200

    Connect to db

--- main.cpp
+++ main.cpp
@@ -2,11 +2,23 @@
 #include <iostream>
 #include <string>
 #include "Generator.h"
+#include "Database.h"
 
 using boost::asio::ip::tcp;
 
+void compute_windowed_mean(std::vector<std::string> inputs, Database &database){
+
+}
+
 
 int main() {
+  std::string connection_uri {"mongodb://localhost:27020"};
+  std::string database_name {"data_engineering_in_cpp_with_manash"};
+
+  Database database(connection_uri, database_name);
+  database.ping();
+
+
   try {
     boost::asio::io_context io_context;
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 5000));
